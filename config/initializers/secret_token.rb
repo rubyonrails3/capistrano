45e90fb10b96d41a197d8afd155fae43c0e666a8
Ismail Akram <rubyonrails3@gmail.com>
2013-03-30 20:42:09 +0500

    Added Post scaffold with title and content:text

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails400::Application.config.secret_key_base = '5c3b2671a9190aca05c9298a75d4eedaf292cc688b7b6eabc4d69aa5ef5d4f395459ba60c4b421b2a8e69fbf828b0390a6b368a228919989f4dee9b8f0273a24'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails400::Application.config.session_store :encrypted_cookie_store, key: '_rails-4_0_0_session'
